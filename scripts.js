f5af5a7317bfa9378d629750ea37da3a7033c2ab
Leinthwirg <niel@wrightclan.com>
2022-11-16 01:50:40 +0000

    added patterns and basic clicks

---
+++ scripts.js
@@ -0,0 +1,13 @@
+window.onload = function() {
+  
+  cardList = document.getElementsByClassName("card"); //collection of cards
+  
+  cardCount = cardList.length; //how many cards are on the table
+  
+  for (c = 0; c < cardCount; c++) {
+       cardList[c].onclick = function() {
+    this.classList.toggle("clicked");
+  }
+ }
+
+}
